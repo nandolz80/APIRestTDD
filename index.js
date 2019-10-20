aef1a3ccec4143ba19f8790799e845544b4b9976
Fernando <fernando.leites@dbccompany.com.br>
2019-10-20 08:17:49 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function hello(){
+    console.log("hello");
+}
+
+const saudacao = () => {
+    var data  = new Date();   
+    return data.getHours() <= 12 ? "Bom dia" : data.getHours() <= 18? "Boa tarde": "Boa noite";
+}
+
+console.log(saudacao());
